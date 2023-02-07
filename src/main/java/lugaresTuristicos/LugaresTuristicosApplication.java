844faea4b85be2070ebb8c0fb8ee2f55638ff3b5
Silvinha <Silvinha@DESKTOP-C5C5H9A>
2023-02-07 15:31:54 -0600

    Meditec primera parte 2

---
+++ src/main/java/lugaresTuristicos/LugaresTuristicosApplication.java
@@ -0,0 +1,13 @@
+package lugaresTuristicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LugaresTuristicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LugaresTuristicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lugaresTuristicos/LugaresTuristicosApplicationTests.java
@@ -0,0 +1,13 @@
+package lugaresTuristicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LugaresTuristicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
